987578db1ab50f66bab477fd3e3b2bf693e76ade
SerbCSharp <serbcsharp@mail.ru>
2024-01-04 20:55:32 +0300

    Добавьте файлы проекта.

---
+++ DiagnosticWebAPIServer/Controllers/DiagnosticController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualBasic;
+
+namespace DiagnosticWebAPIServer.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class DiagnosticController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> PostAsync([FromBody] Update update)
+        {
+            var message = update.Message;
+            if (message != null)
+            {
+                var resultToHtml = await _telegramBotService.DiagnosticAsync(message);
+                var client = new TelegramBotClient(_telegramConfiguration.BotToken);
+                await client.SendTextMessageAsync(message.Chat.Id, resultToHtml, parseMode: ParseMode.Html, null, true);
+            }
+            return Ok();
+        }
+    }
+}
